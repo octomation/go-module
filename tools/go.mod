39bfc02dd082ec354d54129a4c4a35ff82c52ad5
Kamil Samigullin <kamil@samigullin.info>
2020-10-11 17:06:54 +0000

    add goreleaser to toolset

--- tools/go.mod
+++ tools/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/mock v1.4.4
 	github.com/golangci/golangci-lint v1.34.0
 	github.com/kyoh86/looppointer v0.1.7
+	golang.org/x/exp v0.0.0-20201229011636-eab1b5eb1a03
 	golang.org/x/tools v0.5.0
 )
 
--- tools/tools.go
+++ tools/tools.go
@@ -8,10 +8,12 @@ import (
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/kyoh86/looppointer/cmd/looppointer"
+	_ "golang.org/x/exp/cmd/gorelease"
 	_ "golang.org/x/tools/cmd/goimports"
 )
 
 //go:generate go install github.com/golang/mock/mockgen
 //go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
 //go:generate go install github.com/kyoh86/looppointer/cmd/looppointer
+//go:generate go install golang.org/x/exp/cmd/gorelease
 //go:generate go install golang.org/x/tools/cmd/goimports
