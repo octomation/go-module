9693e6d684487ebb4fd279c0c0981c4abc76803f
Kamil Samigullin <kamil@samigullin.info>
2022-02-03 10:51:24 +0300

    ci/cd: try to fix checking build

--- module_test.go
+++ module_test.go
@@ -3,11 +3,13 @@ package module_test
 import (
 	"testing"
 
+	"golang.org/x/time/rate"
+
 	. "go.octolab.org/template/module"
 )
 
 func TestNewModule(t *testing.T) {
-	if NewModule() != "replace me" {
+	if NewModule() != rate.Inf {
 		t.FailNow()
 	}
 }
