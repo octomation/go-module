50146c2d38cd5616baea6c629fcfe1d0978803b2
Kamil Samigullin <kamil@samigullin.info>
2022-04-12 08:43:43 +0300

    issue #83: integrate docz

---
+++ tools/docs/src/components/Logo/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Logo = () => <img
+  src="https://raw.githubusercontent.com/octomation/.github/main/.static/octolab.png"
+  alt="OctoLab"
+/>
---
+++ tools/doczrc.js
@@ -0,0 +1,14 @@
+export default {
+  base: '/go-module/',
+  title: 'OctoLab',
+  description: 'Template for typical module written on Go.',
+
+  src: './docs',
+  files: '**/*.mdx',
+  dest: '../docs',
+  typescript: true,
+
+  themeConfig: {
+    initialColorMode: 'dark',
+  },
+}
